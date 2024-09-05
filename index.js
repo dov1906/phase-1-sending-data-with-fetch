dcf4e8903e6930e35c1a89bcb24189029befa5af
Dov Azogui <dov.azogui@gmail.com>
2024-09-05 16:31:12 -0400

    aaa

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email){
+    const newuser = {
+        name : name,
+        email : email
+    }
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json', 
+        },
+        body: JSON.stringify(newuser),
+    })
+    .then(ser => ser.json())
+    .then(data => {
+
+        const idof = data.id
+        const idelement = document.createElement('p')
+        idelement.textContent =  idof
+        document.body.appendChild(idelement)
+        console.log(data, idelement)
+    })
+    .catch(error=> {
+        const errormessage = document.createElement('p')
+        errormessage.textContent = error.message
+        document.body.appendChild(errormessage)
+        console.log(error.message)
+    })
+
+}
+submitData("dov","data@a")
